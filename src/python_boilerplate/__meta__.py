2a5d6a4ec29ef0acd81fc514a58aab37a55d7aa1
Austin Keller <austin474@gmail.com>
2016-08-24 16:37:51 -0700

    Revert "Fix issues with Python 2 compatibility"
    This reverts commit 5b9e61f9e45ac355099554652db38657e72e8bad.

--- src/python_boilerplate/__meta__.py
+++ src/python_boilerplate/__meta__.py
@@ -1,2 +1,8 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
 __version__ = '0.3.1'
-__author__ = 'F\xc3\xa1bio Mac\xc3\xaado Mendes'
+__author__ = 'Fábio Macêdo Mendes'
--- src/python_boilerplate/core.py
+++ src/python_boilerplate/core.py
@@ -2,11 +2,11 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
-from future.builtins import dict
-from future.builtins import input
+from builtins import dict
+from builtins import input
 from future import standard_library
 standard_library.install_aliases()
-from future.builtins import object
+from builtins import object
 import os
 import datetime
 
